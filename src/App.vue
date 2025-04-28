c75f830c67e3eaea79d71ed06b2c10de4581323b
19zfl <19aleiya@gmail.com>
2025-04-28 22:15:32 +0800

    欢迎日志

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,13 @@
 
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
+import { onMounted } from 'vue'
+const welcomeLog = () => {
+  console.log("欢迎使用lei-picture-frontend！")
+}
+onMounted(() => {
+  welcomeLog()
+})
 </script>
 
 <style>
