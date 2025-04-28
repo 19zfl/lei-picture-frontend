45d6a2aaee2acb88f3e9f8d8b7712f3b5fb2ed09
19zfl <19aleiya@gmail.com>
2025-04-28 22:19:07 +0800

    登录用户状态管理

---
+++ src/stores/useLoginUserStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { getLoginUserUsingGet } from '@/api/userController'
+
+/**
+ * 存出登录用户信息的状态
+ */
+export const useLoginUserStore = defineStore('loginUser', () => {
+  const loginUser = ref<API.LoginUserVO>({
+    userName: '未登录',
+  })
+
+  async function fetchLoginUser() {
+    const res = await getLoginUserUsingGet()
+    if (res.data.code === 0 && res.data.data) {
+      loginUser.value = res.data.data
+    }
+  }
+
+  function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser
+  }
+
+  return { loginUser, setLoginUser, fetchLoginUser }
+})
